37ab06c42edca4144c86842f659c2b3a18d75866
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-06-21 05:44:23 +0000

    created 100-operations.c to help create library

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -5,21 +5,22 @@
  * Returns - values
  */
 int add(int a, int b)
-
+{
 	return (a + b);
-
+}
 int sub(int a, int b)
-
+{
 	return (a - b);
-
+}
 int mul(int a, int b)
-
+{
 	return (a * b);
-
+}
 int div(int a, int b)
-
+{
 	return (a / b);
-
+}
 int mod(int a, int b)
-
+{
 	return (a % b);
+}
