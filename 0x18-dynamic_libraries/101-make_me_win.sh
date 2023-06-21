a8783940385b423f0b5389cff93d5296b546253f
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2023-06-21 10:55:42 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Gideon-Gyebi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libsyringe.so
+wget -P ./ "https://github.com/Gideon-Gyebi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libsyringe.so"
 export LD_PRELOAD=$PWD/../libsyringe.so
